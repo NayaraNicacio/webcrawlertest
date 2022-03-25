80819dab244ddf621b6bc62b2dfc6a1f0102b1d5
NayaraNicacio <nayaranicacio42@gmail.com>
2022-03-24 21:58:28 -0300

    feat: primeiro commit

---
+++ crawler.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+#paginas alvo
+
+urls = ["https://www.vultr.com/pricing/#optimized-cloud-compute",
+            "https://www.digitalocean.com/pricing"
+            ]
+
+for i in urls:
+ print(i)
+
+
+
+
